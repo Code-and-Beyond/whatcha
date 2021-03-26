7daa76333d37e64e9572edfe4c31df8a0eb2c58f
ramjirathore <ramjirathore2802@gmail.com>
2021-03-27 01:14:23 +0530

    header logout button

--- frontend/src/components/Button/Text.js
+++ frontend/src/components/Button/Text.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from 'react'
 
 const TextButton = props => {
 
 	const getClasses = () => {
-		let classes = ["button button--text button--text--primary a a--1 u-c-pointer"];
+		let classes = ["button button--text a a--1 u-c-pointer"]
 
-		if (props.extraStyle) classes.push(props.extraStyle);
+		switch (props.type) {
+			case 1: classes.push("button--no-fill--primary"); break
+			case 2: classes.push("button--no-fill--error"); break
+			case 3: classes.push("button--no-fill--warning"); break
+			case 4: classes.push("button--no-fill--info"); break
+			case 5: classes.push("button--no-fill--success"); break
+			default: classes.push("button--no-fill--normal")
+		}
 
-		return classes.join(' ', ", ");
-	};
+		if (props.extraStyle) classes.push(props.extraStyle)
+
+		return classes.join(' ', ", ")
+	}
 
 	return (
 		<div className={ getClasses() } onClick={ props.onClickHandler }>
 			{props.text }
 		</div>
-	);
-};
+	)
+}
 
-export default TextButton;
+export default TextButton
--- frontend/src/components/Header/PostNav.js
+++ frontend/src/components/Header/PostNav.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Input from '../Input/Input'
 
+import TextButton from '../../components/Button/Text'
+
 import Avatar from '../Icons/Avatar'
 import avatar from '../../assets/profile/me.jpeg'
 
@@ -13,6 +15,7 @@ const PostNavHeader = () => {
 				<h3 className="h h--3 u-m-l-m">Feed</h3>
 				<h3 className="h h--3 u-m-l-m">Trending</h3>
 				<h3 className="h h--3 u-m-l-m">Blogs</h3>
+				<TextButton extraStyle='u-m-l-auto' text='Logout' type={ 1 } />
 			</div>
 			<Input
 				value={ text }
