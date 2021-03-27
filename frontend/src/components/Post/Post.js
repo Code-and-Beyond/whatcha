50895c173dc66b07e841739876e78af7c944a37d
ramjirathore <ramjirathore2802@gmail.com>
2021-03-27 16:20:57 +0530

    post css + some refactoring + toggle comment

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,16 +1,26 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import Avatar from "../Icons/Avatar"
 import Input from '../Input/Input'
 
+import upVote from '../../assets/icons/upvote.svg'
+import upVoteActive from '../../assets/icons/upvote-active.svg'
+import upVotesDone from '../../assets/icons/upvotes-done.svg'
 import ps from '../../assets/profile/prerna.jpg'
 import post1 from '../../assets/posts/post1.jpeg'
 import dots from '../../assets/icons/dots.svg'
+import share from '../../assets/icons/share.svg'
+import comment from '../../assets/icons/comment.svg'
 
 const Post = () => {
 	const [addComment, setAddComment] = useState('')
-	return (
-		<div className='post'>
-			<div className='post__head'>
+	const [upvote, setUpvote] = useState(false)
+	const [openComments, setOpenComments] = useState(false)
+	const [SharePopup, setSharePopup] = useState(false)
+
+
+	const getHeader = () => {
+		return (
+			<React.Fragment>
 				<Avatar src={ ps } alt='avatar' size='7rem' />
 				<div className='post__head--data'>
 					<h3 className='a a--1 a--name'>Prerna Singh</h3>
@@ -20,36 +30,83 @@ const Post = () => {
 				<div className='post__head--dotsContainer u-c-pointer'>
 					<img src={ dots } alt='three dots' className='post__head--dots u-c-pointer' />
 				</div>
-			</div>
+			</React.Fragment>
+		)
+	}
 
-			<p className='a a--2 text--black u-p-t-m'>
-				Ready for your next big step? For a limited time my course “Essential Coding Interview Course - Python Edition” is on SALE!
+	const getContent = () => {
+		return (
+			<React.Fragment>
+				<p className='a a--2 text--black u-p-t-m'>
+					Ready for your next big step? For a limited time my course “Essential Coding Interview Course - Python Edition” is on SALE!
 
-				It’s a tough time for all of us right now, and with the uncertainty with the job market it’s best to be prepared.
+					It’s a tough time for all of us right now, and with the uncertainty with the job market it’s best to be prepared.
 
-				This course is design your help you to be prepare with your next job interview, with specially chosen practical coding exercises.
+					This course is design your help you to be prepare with your next job interview, with specially chosen practical coding exercises.
 
-				My goal is to help you to be prepared for your next coding interview and get you the best possible job offer you deserve!
+					My goal is to help you to be prepared for your next coding interview and get you the best possible job offer you deserve!
 
-				So why not join today and get ready for your next adventure!
+					So why not join today and get ready for your next adventure!
 
-				https://lnkd.in/eV2Ckji
+					https://lnkd.in/eV2Ckji
 
-				SHARE and TAG so that others can also learn.
+					SHARE and TAG so that others can also learn.
 
-				#softwareengineers #softwareengineering
+					#softwareengineers #softwareengineering
 
-				Follow Nathan Clarke, AMBCS RITTech
+					Follow Nathan Clarke, AMBCS RITTech
 			</p>
-			<div>
-				<img src={ post1 } className='post__image' alt='post content' />
+				<div>
+					<img src={ post1 } className='post__image' alt='post content' />
+				</div>
+			</React.Fragment>
+		)
+	}
+
+	const getIcons = () => {
+		return (
+			<React.Fragment>
+				<div className='post__icons--basic'>
+					<span onClick={ () => setUpvote(!upvote) }>
+						{ upvote ?
+							<img src={ upVote } className='post__icons--upvote' alt='upvote' /> : <img src={ upVoteActive } className='post__icons--upvote' alt='upvote' />
+						}
+						<h3 className='a a--2 a--name text--black'>Upvote</h3>
+					</span>
+					<span onClick={ () => setOpenComments(!openComments) }>
+						<img src={ comment } className='post__icons--share' alt='upvote' />
+						<h3 className='a a--2 a--name text--black'>Comment</h3>
+					</span>
+					<span onClick={ () => setSharePopup(true) }>
+						<img src={ share } className='post__icons--share' alt='upvote' />
+						<h3 className='a a--2 a--name text--black'>Share</h3>
+					</span>
+				</div>
+				<div className='b b--3 text--black d--f ai--c'>
+					<img src={ upVotesDone } className='post__icons--upvotesDone' alt='upvote' /> 2 upvotes - 2 comments
+				</div>
+			</React.Fragment>
+		)
+	}
+
+	return (
+		<div className='post'>
+			<div className='post__head'>
+				{ getHeader() }
 			</div>
-			<Input
-				value={ addComment }
-				handleInput={ (val) => setAddComment(val) }
-				placeholder='Add a comment'
-				extraStyle='post__input'
-			/>
+			{getContent() }
+			<div className='post__icons'>
+				{ getIcons() }
+			</div>
+			{openComments ?
+				<Input
+					value={ addComment }
+					handleInput={ (val) => setAddComment(val) }
+					placeholder='Add a comment'
+					extraStyle='post__input'
+				/>
+				: null
+			}
 		</div>
 	)
 }
