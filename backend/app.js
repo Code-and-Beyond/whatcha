366df93d3d2c246c64c95dbe1a0c8c89f20b86d1
ramjirathore <ramjirathore2802@gmail.com>
2021-03-29 03:48:08 +0530

    backend merge

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const cors = require('cors');
-const https = require('https');
-const jwt = require('jsonwebtoken');
-const bodyparser = require('body-parser');
-const axios = require('axios');
-const connection = require('./connections/user');
-const auth = require('./helpers/auth');
+const express = require('express')
+const cors = require('cors')
+const https = require('https')
+const jwt = require('jsonwebtoken')
+const bodyparser = require('body-parser')
+const axios = require('axios')
+const connection = require('./connections/user')
+const auth = require('./helpers/auth')
 
 
 
@@ -18,15 +18,15 @@ const server = http.createServer(app)
 server.listen(5000, () => console.log("server is running on port 5000"))
 
 var checkUserFilter = function (req, res, nex) {
-	const regex = "\/api\/pvt\/";
-	if (req._parsedUrl.pathname.match(regex)) { auth.authenticateToken(req, res, nex); }
-	else { nex(); }
-};
+	const regex = "\/api\/pvt\/"
+	if (req._parsedUrl.pathname.match(regex)) { auth.authenticateToken(req, res, nex) }
+	else { nex() }
+}
 
-app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(checkUserFilter);
+app.use(cors())
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(checkUserFilter)
 
 require('./routes/auth/google')(app, connection, axios)
 
