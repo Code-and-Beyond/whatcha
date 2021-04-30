bb34fdf9b1d8956c7365a9eb09d3fc3a49b4d198
ramjirathore <ramjirathore2802@gmail.com>
2021-04-30 20:11:33 +0530

    blogs create switch

--- frontend/src/components/Header/Features.js
+++ frontend/src/components/Header/Features.js
@@ -1,9 +1,10 @@
 import Avatar from '../Avatar/Avatar';
 import { getUser } from '../../helpers/session';
 import { useDispatch } from 'react-redux';
-import { showChats } from '../../store/actions/index';
+import { showChats, showCreateBlogs } from '../../store/actions/index';
 import logo from '../../assets/Logos/company_logo.png';
 import chat from '../../assets/icons/chat.svg';
+import write from '../../assets/icons/pencil.svg';
 
 const FeaturesHeader = () => {
 	const dispatch = useDispatch();
@@ -16,7 +17,9 @@ const FeaturesHeader = () => {
 					<Avatar src={ getUser().image } extraStyle="u-c-pointer" alt="profile logo" />
 					: null
 			}
+			<div className='header__line' />
 			<Avatar src={ chat } bg noRad extraStyle="u-c-pointer" alt="Chats logo" size='3.5rem' handleClick={ () => dispatch(showChats()) } />
+			<Avatar src={ write } bg noRad extraStyle="u-c-pointer" alt="Chats logo" size='3.5rem' handleClick={ () => dispatch(showCreateBlogs()) } />
 			<h1>Icon2</h1>
 			<h1>Icon3</h1>
 		</div>
