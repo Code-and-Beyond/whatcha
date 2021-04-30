d8f42e2d4b13ff3716a2096f04409fa4d53e2af3
ramjirathore <ramjirathore2802@gmail.com>
2021-05-01 05:26:39 +0530

    keyboard added

--- frontend/src/components/Chat/ChatSpace.js
+++ frontend/src/components/Chat/ChatSpace.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+import TextBox from '../TextBox/TextBox';
+
+import IconContainer from '../Icon/Container';
+
+import sendButton from '../../assets/icons/send-button.svg';
 
 const ChatSpace = () => {
+	const [text, setText] = useState('');
+
+	const handleTextChange = (message) => {
+		setText(message);
+	};
+
 	return (
 		<div className='chat__space'>
-			Hi'm chat space
+			<h1 className='b b--3'>
+				Hi'm chat space
+			</h1>
+			<div className='chat__space--keyboard'>
+				<TextBox value={ text } placeholder='Type a message' rows={ 1 } handleChange={ handleTextChange } />
+				<IconContainer src={ sendButton } extraStyleI='chat__space--keyboard-send icon--s' />
+			</div>
 		</div>
 	);
 };
--- frontend/src/components/Icon/Container.jsx
+++ frontend/src/components/Icon/Container.jsx
@@ -4,13 +4,27 @@ import Icon from './Icon';
 
 const IconContainer = (props) => {
 	const { text, color, src, alt } = props;
+
+	const getContainerClasses = () => {
+		let classesContainer = ['icon--container u-c-pointer'];
+		if (props.extraStyleC) classesContainer.push(props.extraStyleC);
+		return classesContainer.join(' ', ', ');
+	};
+
+	const getIconClasses = () => {
+		let classesIcon = ['icon--m'];
+		if (props.extraStyleI) classesIcon.push(props.extraStyleI);
+		return classesIcon.join(' ', ', ');
+
+	};
+
 	return (
 		<div
-			className="icon--container u-c-pointer"
+			className={ getContainerClasses() }
 			style={ { background: color } }
 			onClick={ props.onClickHandler }
 		>
-			<Icon extraStyle="icon--m" src={ src } alt={ alt } />
+			<Icon extraStyle={ getIconClasses() } src={ src } alt={ alt } />
 			<div className="d__w--100 d--f ai--c jc--c">
 				<h1 className="h--white h h--3 u-c-pointer">{ text }</h1>
 			</div>
