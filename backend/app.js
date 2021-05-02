92153da33c9d953110db1621eea760c1b8a79f0d
Hemant <hemant2132@gmail.com>
2021-05-02 23:28:41 +0530

    Fix: Handle image size on server

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const https = require('https');
 const jwt = require('jsonwebtoken');
-const bodyparser = require('body-parser');
 const axios = require('axios');
 const connection = require('./connections/user');
 const auth = require('./helpers/auth');
@@ -21,8 +20,8 @@ var checkUserFilter = function (req, res, nex) {
 };
 
 app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(checkUserFilter);
 
 // app.use((req, res, next) => {
