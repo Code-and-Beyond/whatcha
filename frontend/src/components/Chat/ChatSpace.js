df6fa5146e8dcd7f16778ea2a85a08901a19c2e3
Hemant <hemant2132@gmail.com>
2021-05-05 17:47:19 +0530

    Feat: Handle chat rooms on follow/unfollow

--- frontend/src/components/Chat/ChatSpace.js
+++ frontend/src/components/Chat/ChatSpace.js
@@ -10,122 +10,124 @@ import axios from 'axios';
 
 let socket;
 const ChatSpace = ({ chatRoom, currentUserId }) => {
-	const [text, setText] = useState('');
-	const [receiverSocket, setReceiverSocket] = useState(null);
-	const [messages, setMessages] = useState([]);
-	const [initialised, setInitialised] = useState(false);
+    const [text, setText] = useState('');
+    const [receiverSocket, setReceiverSocket] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [initialised, setInitialised] = useState(false);
 
-	const ENDPOINT = 'http://localhost:8080';
-	useEffect(() => {
-		if (!initialised) {
-			axios({
-				method: 'GET',
-				url: `${ENDPOINT}/api/pub/chat/${chatRoom.chatRoomId}/messages`,
-			})
-				.then((res) => {
-					if (res.status === 200 && !res.data.error) {
-						// console.log(res.data.data);
-						setMessages(res.data.data);
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				});
+    const ENDPOINT = 'http://localhost:8080';
+    useEffect(() => {
+        if (!initialised) {
+            axios({
+                method: 'GET',
+                url: `${ENDPOINT}/api/pub/chat/${chatRoom.chatRoomId}/messages`,
+            })
+                .then((res) => {
+                    if (res.status === 200 && !res.data.error) {
+                        // console.log(res.data.data);
+                        setMessages(res.data.data);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
 
-			socket = io(ENDPOINT);
+            socket = io(ENDPOINT);
 
-			socket.emit(
-				'join',
-				{ userId: currentUserId, chatRoomId: chatRoom.chatRoomId },
-				(resChatRoom) => {
-					if (
-						currentUserId !== resChatRoom.userOne &&
-						resChatRoom.socketOne !== undefined
-					)
-						setReceiverSocket(resChatRoom.socketOne);
-					else if (
-						currentUserId !== resChatRoom.userTwo &&
-						resChatRoom.socketTwo !== undefined
-					)
-						setReceiverSocket(resChatRoom.socketTwo);
-				}
-			);
-			setInitialised(true);
-		}
+            socket.emit(
+                'join',
+                { userId: currentUserId, chatRoomId: chatRoom.chatRoomId },
+                (resChatRoom) => {
+                    if (
+                        currentUserId !== resChatRoom.userOne &&
+                        resChatRoom.socketOne !== undefined
+                    )
+                        setReceiverSocket(resChatRoom.socketOne);
+                    else if (
+                        currentUserId !== resChatRoom.userTwo &&
+                        resChatRoom.socketTwo !== undefined
+                    )
+                        setReceiverSocket(resChatRoom.socketTwo);
+                }
+            );
+            setInitialised(true);
+        }
 
-		socket.on('receiverAvailable', (receiverSocket) => {
-			setReceiverSocket(receiverSocket);
-		});
+        socket.on('receiverAvailable', (receiverSocket) => {
+            setReceiverSocket(receiverSocket);
+        });
 
-		socket.on('message', (newMessage) => {
-			// console.log(socket.id, 'message');
-			setMessages([...messages, newMessage]);
-		});
-	}, [chatRoom.chatRoomId, currentUserId, messages, initialised]);
+        socket.on('message', (newMessage) => {
+            // console.log(socket.id, 'message');
+            setMessages([...messages, newMessage]);
+        });
+    }, [chatRoom.chatRoomId, currentUserId, messages, initialised]);
 
+    const handleSend = () => {
+        const messageObj = {
+            chatRoomId: chatRoom.chatRoomId,
+            sender: currentUserId,
+            receiver:
+                currentUserId === chatRoom.userOne
+                    ? chatRoom.userTwo
+                    : chatRoom.userOne,
+            receiverSocket,
+            text,
+        };
+        socket.emit('sendMessage', { message: messageObj });
+        setText('');
+    };
 
+    const handleTextChange = (message, e) => {
+        setText(message);
+    };
 
-	const handleSend = () => {
-		const messageObj = {
-			chatRoomId: chatRoom.chatRoomId,
-			sender: currentUserId,
-			receiver:
-				currentUserId === chatRoom.userOne
-					? chatRoom.userTwo
-					: chatRoom.userOne,
-			receiverSocket,
-			text,
-		};
-		socket.emit('sendMessage', { message: messageObj });
-		setText('');
-	};
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            handleSend();
+        }
+    };
 
-	const handleTextChange = (message, e) => {
-		setText(message);
-	};
-
-	const handleKeyPress = (e) => {
-		if (e.keyCode === 13 && !e.shiftKey) {
-			handleSend();
-		}
-	};
-
-	return (
-		<div className="chat__space">
-			<div className="chat__space--messages u-p-v-s u-p-h-m">
-				{ messages.map((message) =>
-					message.sender === currentUserId ? (
-						<Message
-							key={ message.messageId }
-							text={ message.text }
-							time={ message.time }
-						/>
-					) : (
-						<Message
-							key={ message.messageId }
-							receive
-							text={ message.text }
-							time={ message.time }
-						/>
-					)
-				) }
-			</div>
-			<div className="chat__space--keyboard">
-				<TextBox
-					value={ text }
-					placeholder="Type a message"
-					rows={ 1 }
-					handleChange={ handleTextChange }
-					handleKeyPress={ handleKeyPress }
-				/>
-				<IconContainer
-					src={ sendButton }
-					onClickHandler={ handleSend }
-					extraStyleI="chat__space--keyboard-send icon--s"
-				/>
-			</div>
-		</div>
-	);
+    return (
+        <div className="chat__space">
+            <div className="chat__space--messages u-p-v-s u-p-h-m">
+                {messages.map((message) =>
+                    message.sender === currentUserId ? (
+                        <Message
+                            key={message.messageId}
+                            text={message.text}
+                            time={message.time}
+                        />
+                    ) : (
+                        <Message
+                            key={message.messageId}
+                            receive
+                            text={message.text}
+                            time={message.time}
+                        />
+                    )
+                )}
+            </div>
+            {chatRoom.connectionExists ? (
+                <div className="chat__space--keyboard">
+                    <TextBox
+                        value={text}
+                        placeholder="Type a message"
+                        rows={1}
+                        handleChange={handleTextChange}
+                        handleKeyPress={handleKeyPress}
+                    />
+                    <IconContainer
+                        src={sendButton}
+                        onClickHandler={handleSend}
+                        extraStyleI="chat__space--keyboard-send icon--s"
+                    />
+                </div>
+            ) : (
+                <h1 className="b b--3 text--center u-p-v-s h--disabled">{`${chatRoom.fullname} is no longer a connection.`}</h1>
+            )}
+        </div>
+    );
 };
 
 export default ChatSpace;
--- frontend/src/store/actions/index.js
+++ frontend/src/store/actions/index.js
@@ -1,15 +1,29 @@
 export { toggleLoading } from './loading';
 export { toggleLoginModal } from './loginModal';
 export {
-	toggleLoggedIn,
-	fetchUserConnectionsStart,
-	fetchUserConnectionsSuccess,
-	fetchUserConnectionsFailed,
-	initUserConnections,
-	initAllUsers,
-	followUser,
-	unfollowUser,
+    toggleLoggedIn,
+    fetchUserConnectionsStart,
+    fetchUserConnectionsSuccess,
+    fetchUserConnectionsFailed,
+    initUserConnections,
+    initAllUsers,
+    followUser,
+    unfollowUser,
 } from './user';
-export { showChats, showCreateBlogs, fetchChatsRooms, createChatRoom } from './connect';
+export {
+    showChats,
+    showCreateBlogs,
+    fetchChatsRooms,
+    createChatRoom,
+    updateChatRoomByUsers,
+} from './connect';
 export { showProfile, showConnections } from './features';
-export { showFeed, showTrending, showBlogs, showSavedPosts, fetchAllPosts, fetchTrendingPosts, fetchBlogs } from './posts';
+export {
+    showFeed,
+    showTrending,
+    showBlogs,
+    showSavedPosts,
+    fetchAllPosts,
+    fetchTrendingPosts,
+    fetchBlogs,
+} from './posts';
