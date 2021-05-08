a594acf1ba36e3ef4cdc55620271781d032e8a62
Hemant <hemant2132@gmail.com>
2021-05-08 12:20:57 +0530

    Fix: Connection pool for mysql

--- backend/connections/user.js
+++ backend/connections/user.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql')
 
-let userConnection = mysql.createConnection({
+let userConnection = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	database: process.env.DB_DATABASE,
 	password: process.env.DB_PASS
 })
 
-userConnection.connect(function (err) {
-	if (err) {
-		console.error('Error connecting: ' + err.stack)
-		return
-	}
-})
+// userConnection.connect(function (err) {
+// 	if (err) {
+// 		console.error('Error connecting: ' + err.stack)
+// 		return
+// 	}
+// })
 
 module.exports = userConnection
